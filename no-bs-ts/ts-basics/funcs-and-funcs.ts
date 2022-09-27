98251fae78c072964bfe37023ff6627365b4ee0d
Mark Harmon <neothej1sec@gmail.com>
2022-09-26 23:43:13 -0500

    finished working on functions that return functions

---
+++ no-bs-ts/ts-basics/funcs-and-funcs.ts
@@ -0,0 +1,25 @@
+export function printToFile(text: string, callback: () => void):void{
+    console.log(text);
+    callback();
+}
+
+export type MutationFunction = (v: number) => number;
+// decare a function inside a function with a type^^
+// export function arrayMutate(numbers: number[], mutate: (v: number) => number): number[] {
+//     return numbers.map(mutate)
+// }
+
+export function arrayMutate(numbers: number[], mutate: MutationFunction): number[] {
+    return numbers.map(mutate)
+}
+
+const myNewNutate: MutationFunction = (v: number) => v * 100;
+
+// console.log(arrayMutate([1,2,3], (v) => v * 10))
+
+export type NewAdderTypeFunc = (v: number) => number;
+export function createAdder(num: number): NewAdderTypeFunc{
+    return (val: number) => num + val
+}
+const addOne = createAdder(5);
+console.log(addOne(55))
