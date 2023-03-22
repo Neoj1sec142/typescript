3b33708740d091b51b15f75894413b443f4e6781
Mark Harmon <neothej1sec@gmail.com>
2023-03-22 13:59:09 -0500

    inital frontend

---
+++ GraphQL/book-app/client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import BookList from './components/BookList'
+
+const App = () => {
+  return (
+    <div id="main">
+      <h1>Neo's Reading List</h1>
+      <BookList />
+    </div>
+  )
+}
+
+export default App
